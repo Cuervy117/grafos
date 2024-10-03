11123fd52d5beab60b881f93442645052766ab92
Samuel Santiago Cruz Macedo <santi.sam120@gmail.com>
2024-10-03 16:44:27 -0600

    Actualizando busquedas con bfs

---
+++ busquedas/BFS.java
@@ -0,0 +1,28 @@
+package algoritmos;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFS {
+    public static List<Integer> busquedaExpansion(List<List<Integer>> grafo, int verticeInicial){
+        // Se inicializan las listas que utilizaremos
+        Queue<List<Integer>> queue = new LinkedList<>();
+        List<Integer> visitados = new ArrayList<>(); //Para guardar nodos adyacentes
+
+        List <Integer> nodo = grafo.get(verticeInicial);
+        System.out.println();
+        queue.add(nodo); // Se agrega el nodo inicial
+        visitados.add(verticeInicial); //Se marca ese nodo como visitado
+        while (!queue.isEmpty()) {
+            List <Integer> v = queue.poll(); // Se almacena el nodo y se desencola
+            for(Integer w : v){
+                if(!visitados.contains(w)){ // Si w no está marcado como visitado...
+                    queue.add(grafo.get(w));
+                    visitados.add(w);
+                } 
+            }
+        }
+        return visitados;
+    }
+}
